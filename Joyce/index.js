2e848c874ebf8c195ddd95d9958dc01754e9f66f
Joyce <puch1996@gmail.com>
2020-10-27 19:59:56 +0100

    done

--- Joyce/index.js
+++ Joyce/index.js
@@ -48,10 +48,55 @@ for (let i = 1; i < 1000; i++) {
 }
 
 //7. Write programs that produce the following outputs:
-//100 200 300 400 500 600 700 800 900 1000
 
+//100 200 300 400 500 600 700 800 900 1000
 let x = [];
 for (let i = 1; i <= 10; i++) {
   x.push(`${i}00`);
 }
 console.log(x.join(" "));
+
+// 1 2 4 8 16 32 64 128
+let x1 = [];
+for (let i = 1; i <= 8; i++) {
+  x1.push(2 ** i);
+}
+console.log(x1.join(" "));
+
+// 0 2 4 6 8 10
+let x2 = [];
+for (let i = 0; i <= 5; i++) {
+  x2.push(2 * i);
+}
+console.log(x2.join(" "));
+
+// 3 6 9 12 15
+let x3 = [];
+for (let i = 1; i <= 15; i++) {
+  if (i % 3 == 0) {
+    x3.push(i);
+  }
+}
+console.log(x3.join(" "));
+
+// 9 8 7 6 5 4 3 2 1 0
+let x4 = [];
+for (let i = 0; i < 10; i++) {
+  x4.push(i);
+}
+x4.reverse();
+console.log(x4.join(" "));
+
+// 1 1 1 2 2 2 3 3 3 4 4 4
+let x5 = [];
+for (let i = 1; i <= 4; i++) {
+  x5.push(`${i} ${i} ${i}`);
+}
+console.log(x5.join(" "));
+
+// 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4
+let x6 = [];
+for (let i = 0; i <= 4; i++) {
+  x6.push(`${i}`);
+}
+console.log(x6.join(" "), x6.join(" "), x6.join(" "));
