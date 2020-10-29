f7888e2fccc5e4ef4dc57075853e608402f6775e
Tomas <eliz.tomas23@gmail.com>
2020-10-29 14:26:50 +0100

    nr.7 added

--- Tomas/script.js
+++ Tomas/script.js
@@ -25,12 +25,12 @@ for (i = 0; i <= 20; i++) {
 
 // Write a program that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18"). Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total)
 for (i = 0; i <= 10; i++) {
-    log(i * 9);
-    for(x = 1; x <=10; x++) {
-       if(i > 0) {
-           let multiResult = x * i;
-           log(`${x} multiplied by ${i} = ${multiResult}`);
-       }
+  log(i * 9);
+  for (x = 1; x <= 10; x++) {
+    if (i > 0) {
+      let multiResult = x * i;
+      log(`${x} multiplied by ${i} = ${multiResult}`);
+    }
   }
 }
 
@@ -40,8 +40,7 @@ let output2 = "Buzz";
 for (i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     log(output1 + output2);
-  }
-  else if (i % 3 === 0) {
+  } else if (i % 3 === 0) {
     log(output1);
   } else if (i % 5 === 0) {
     log(output2);
@@ -51,16 +50,70 @@ for (i = 1; i <= 100; i++) {
 }
 
 // Write a program to add the multiples of 3 and 5 under 1000
-for(i = 15; i < 1000; i++) {
-   if(i % 3 === 0 && i % 5 === 0) {
-       log(i);
-   }
+for (i = 15; i < 1000; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    log(i);
+  }
 }
 
+// Write programs that produce the following outputs:
+// 1.
+const numbers = [];
+for (let i = 100; i <= 1000; i += 100) {
+  numbers.push(i);
+}
+log("numbers 1 -- >", numbers.toString());
+
+// 2.
+const numbers2 = [];
+for (i = 1; i <= 128; i *= 2) {
+  numbers2.push(i);
+}
+log("numbers 2 -->", numbers2.toString());
+
+// 3.
+const numbers3 = [];
+for (i = 0; i <= 10; i += 2) {
+  numbers3.push(i);
+}
+log("numbers 3 -->", numbers3.toString());
+
+// 4.
+const numbers4 = [];
+for (i = 3; i <= 15; i += 3) {
+  numbers4.push(i);
+}
+log("numbers 4 -->", numbers4.toString());
+
+// 5.
+const numbers5 = [];
+for (i = 9; i >= 0; i--) {
+  numbers5.push(i);
+}
+log("numbers 5 -->", numbers5.toString());
+
+// 6.
+const numbers6 = [];
+for (i = 1; i <= 4; i++) {
+  numbers6.push(i);
+  numbers6.push(i);
+  numbers6.push(i);
+}
+log("numbers 6 -->", numbers6.toString());
+
+// 7.
+const numbers7 = [];
+const cluster = [];
+for (i = 0; i <= 4; i++) {
+  numbers7.push(i);
+  cluster.push(numbers7);
+}
+log("numbers 7 -->", cluster.toString().slice(0, 29));
+
 // Write a program to check whether a word is a palindrome or not
-let str = 'racecar';
-const strToArr = str.split('');
-let newStr = strToArr.reverse().join('');
-if(str === newStr) {
-    log(`${str} --> a palindrome`)
+let str = "racecar";
+const strToArr = str.split("");
+let newStr = strToArr.reverse().join("");
+if (str === newStr) {
+  log(`${str} --> a palindrome`);
 }
